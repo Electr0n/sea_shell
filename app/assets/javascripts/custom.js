ba7b3359ed0da23bae278266934a463e7fa8b0c4
Electr0n <skorodumov.eugene@gmail.com>
2017-03-28 14:15:05 +0300

    001 - footer rework

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,7 @@
+// $(document).ready(function() {
+//   var docH = $(window).height();
+//   var contentH = $('#footer').position().top + $('#footer').height();
+//   if (contentH < docH) {
+//     $('#footer').css("margin-top", (docH - contentH) + 'px');
+//   }
+// });
