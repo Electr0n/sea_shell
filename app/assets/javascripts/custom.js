b90b62a52380b250188ec2befd987054097c0828
Electr0n <hunterlzp@yandex.ru>
2017-03-29 23:40:37 +0300

    004 one page app - added script to activate && open current link - added partials - fixed footer

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,17 +1,39 @@
-// $(document).ready(function() {
-//   var docH = $(window).height();
-//   var contentH = $('#footer').position().top + $('#footer').height();
-//   if (contentH < docH) {
-//     $('#footer').css("margin-top", (docH - contentH) + 'px');
-//   }
-// });
-
 $(document).ready(function(){
+  // for location hash
+  var l;
+  var check_url = function (){
+    l = $("a[href='" + window.location.hash + "']")
+    l.trigger('click');
+    if (l.parent().prop('tagName')=='LI'){
+      $('.panel-collapse').removeClass('in');
+      l.parent().parent().parent().addClass('in');
+    }
+    else{
+      l.parent().parent().addClass('active');
+    }
+  }
+  // when open page first time with hash
+  if (window.location.hash.length == 0) {
+    $("a[href='#main']").parent().parent().addClass('active');
+  } 
+  else {
+    check_url();
+  }
+  // when change url
+  $(window).on('hashchange', function() {
+    check_url();
+  });
   $(".panel.panel-default").find("a").click(function(){
+    // add hash to url
     if ($(this).attr('href').indexOf("collapse") < 0) {
       window.location.hash = ($(this).attr('href'));
-      $(this).parent().parent().addClass('active');
-      $(".in").removeClass('in');
+      // clean active classes
+      $('.panel-heading').removeClass('active');
+      $('.list-group-item').removeClass('active');
+      // if this header-link add active class
+      if ($(this).parent().prop('tagName') != 'LI'){
+        $(this).parent().parent().addClass('active');
+      }
     }
   });
 });
